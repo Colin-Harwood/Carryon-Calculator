637b0840c19d9a085e72ac8faa143675fcf3b41d
Colin Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-05-20 22:38:41 +0200

    Set up tailwind

---
+++ Carryon Calculator/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+        <div class="text-8xl font-bold">
+            TEst
+        </div>
+        <h1 class="text-4xl">hello</h1>
+    </>
+    
+  )
+}
+
+export default Home
