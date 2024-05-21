60ab21418dcf4556db13238c9237f7bad992cbd7
Colin Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-05-21 20:14:44 +0200

    Centered and gave the information field a set background and border

--- Carryon Calculator/src/pages/Home.jsx
+++ Carryon Calculator/src/pages/Home.jsx
@@ -3,18 +3,24 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-        <div className="flex justify-center text-8xl font-bold w-full">
+    <div className="bg-blue-500 min-h-screen flex flex-col">
+        <div className="flex justify-center text-8xl font-bold w-full text-white mt-8">
             Carryon Calculator
         </div>
-        <div className="bg-blue-500 flex flex-col items-center justify-center text-center min-h-screen">
-          <h2 class="text-5xl mb-8">Enter your bag details:</h2>
-          <form className="flex flex-col w-1/2 items-center">
-            <input type="number" className="mb-4 p-2 border rounded w-2/3" placeholder="Length" />
-            <input type="number" className="mb-4 p-2 border rounded w-2/3" placeholder="Width" />
-            <input type="number" className="mb-4 p-2 border rounded w-2/3" placeholder="Height" />
-            <input type="submit" className="border rounded bg-white p-3 w-1/2 " value="Submit"/>
-          </form>
+        <div className="flex grow items-center justify-center text-center">
+          <div class="h-auto bg-blue-200 w-1/2 p-10 rounded-xl border-blue-100 border-8">
+            <h2 class="text-5xl mb-8">Enter your bag details:</h2>
+            
+            <form className="flex flex-col w-full items-center">
+              <input type="number" className="mb-4 p-2 border rounded" placeholder="Length" />
+              <input type="number" className="mb-4 p-2 border rounded" placeholder="Width" />
+              <input type="number" className="mb-4 p-2 border rounded" placeholder="Height" />
+              <input type="submit" className="border rounded bg-white p-3" value="Submit"/>
+            </form>
+          </div>
         </div>
+    </div>
+        
     </>
   )
 }
